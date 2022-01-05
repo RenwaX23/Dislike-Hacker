c03a91f35b28b8d4d94f6393b92b2852a95ed30d
Renwa <renwax23@gmail.com>
2022-01-06 00:54:44 +0300

    Add files via upload

---
+++ contentscript.js
@@ -0,0 +1,37 @@
+var element = ''
+
+setTimeout(function() {
+    for (const a of document.querySelectorAll("yt-formatted-string")) {
+        if (a.textContent=="Dislike") {
+            element = a
+            break;
+        }
+    }
+}, 1000)
+
+async function start() {
+
+    setTimeout(function() {
+        element.innerText = 'Loading'
+    }, 1000)
+
+    videoId = new URL(location).searchParams.get('v')
+
+    await fetch('https://floating-citadel-65780.herokuapp.com/?video=' + videoId).then((k) => k.json()).then((k) => {
+        console.log(k)
+        element.innerText = k.count
+
+    })
+
+}
+
+let lastUrl = location.href;
+new MutationObserver(() => {
+    const url = location.href;
+    if (url !== lastUrl) {
+        lastUrl = url;
+        start();
+    }
+}).observe(document, { subtree: true, childList: true });
+
+start()
